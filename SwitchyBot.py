a7f5181b03ddc88c376b5d57a0e865f483f383ee
Jamie Hoogeveen <theforestcommando@gmail.com>
2023-02-23 16:18:41 +0100

    Fixed?

--- SwitchyBot.py
+++ SwitchyBot.py
@@ -1,11 +1,26 @@
 from termcolor import cprint
+from enum import Enum
 
 import pexpect
 import pygatt
+import queue
 import re
 
+handle_queue = queue.Queue
+
+class ActionStatus(Enum):
+    complete = 1
+    device_busy = 3
+    device_unreachable = 11
+    device_encrypted  = 7
+    device_unencrypted = 8
+    wrong_password = 9
+
+    unable_resp = 254
+    unable_connect = 255
+
 class Bot(object):
-    """Switchbot class to control the bot."""
+    "Switchbot class to control the bot."
 
     def __init__(self, bot_id: int, mac: str, name: str):
 
@@ -22,6 +37,7 @@ class Bot(object):
         self.notification_activated = False
         cprint(f"Succesfully created {self.name} at {self.mac} with id {self.bot_id}", "cyan")
 
+
     def connect(self):
         con = pexpect.spawn('gatttool -b ' + self.mac + ' -t random -I')
         print('Preparing to connect')
@@ -37,6 +53,7 @@ class Bot(object):
                 return
             cprint(f"Connected to {self.name} at {self.mac}", "cyan")
 
+
     def press(self):
         try:
             self.adapter.start()
@@ -44,21 +61,47 @@ class Bot(object):
 
             cmd = b'\x57\x01'
             value = self.write(handle=0x16, cmd=cmd)
+            self.handle_notify(value=value)
 
         finally:
             self.adapter.stop()
 
+
     def _connect(self):
+        print("Attempting reconnection")
         try:
-            self.device = self.adapter.connect(self.mac, address_type=pygatt.BLEAddressType.random)
+            self.device = self.adapter.connect(self.mac, address_type = pygatt.BLEAddressType.random)
             cprint(f"Succesfully reconnected to {self.name} at {self.mac}", "cyan")
         except pygatt.BLEError:
-            raise ConnectionRefusedError(f"Failed to connect to {self.name} at {self.mac}")
+            raise ConnectionError(f"Failed to connect to {self.name} at {self.mac}")
 
-    def write(self, handle, cmd):
+    def notify(self):
+        uuid = "cba20003-224d-11e6-9fb8-0002a5d5c51b"
+        try:
+            self.device.subscribe(uuid, callback=handle_notify)
+            self.notification_activated = True
+        except pygatt.BLEError:
+            raise ConnectionError("Failed to activate notification")
+
+
+    def handle_notify(handle: int, value: bytes):
+        handle_queue.put((handle, value))
+
+
+    def write(self, handle, cmd, timeout = 5):
+        print("Attempting to send command")
         try:
             self.device.char_write_handle(handle = handle, value = cmd)
-            cprint(f"Succesfully send {cmd} to {self.name} using handle {handle}")
+            _, value = handle_queue.get(timeout=timeout)
+            cprint(f"Succesfully sent {cmd} to {self.name} using handle {handle}")
  
         except pygatt.BLEError:
-            raise ConnectionRefusedError(f"Failed to write {cmd} to {self.name} at {self.mac}")
+            raise ConnectionError(f"Failed to sent {cmd} to {self.name} at {self.mac}")
+
+    
+    def _handle_switchbot_status_msg(self, value: bytearray):
+        status = value[0]
+        action_status = ActionStatus(status)
+
+        if action_status is not ActionStatus.complete:
+            raise ConnectionError(f"{self.name} failed to execute command")
